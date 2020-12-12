1854df38000d8bfa32c8feb1aae565742e8a136d
Vũ Tuân <75652084+vuhuyentuan@users.noreply.github.com>
2020-12-12 12:16:55 +0700

    Update PageController.php

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,28 +6,11 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-<<<<<<< HEAD
-   public function getIndex (){
-        return view('layout_index.index');
-    }
-
-   public function getchitiet (){
-        return view('layout_index.page.product_simgle');
-    }
-
-   public function gettintuc (){
-        return view('layout_index.news');
-    }
-
-    public function gettatcaSP (){
-        return view('layout_index.page.viewall');
-=======
     public function getIndex(){
         return view('layout_index.index');
     }
 
     public function getChitiet(){
-        return view('layout_index.page.product_detail');
->>>>>>> feb25c6e06c22ebb88153777f89cd78ad15d11b8
+        return view('layout_index.page.product');
     }
 }
