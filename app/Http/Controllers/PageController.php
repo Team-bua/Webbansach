9c8350db450c9f05b52b391332b31d904f86280b
Lâm Nguyễn <lamnguyen041297@gmail.com>
2020-12-12 12:20:49 +0700

    baichinh

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-<<<<<<< HEAD
    public function getIndex (){
         return view('layout_index.index');
     }
@@ -21,13 +20,5 @@ class PageController extends Controller
 
     public function gettatcaSP (){
         return view('layout_index.page.viewall');
-=======
-    public function getIndex(){
-        return view('layout_index.index');
-    }
-
-    public function getChitiet(){
-        return view('layout_index.page.product_detail');
->>>>>>> feb25c6e06c22ebb88153777f89cd78ad15d11b8
     }
 }
