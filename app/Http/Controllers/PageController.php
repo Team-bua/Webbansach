6d26006532d43283d7c9a78f74f13741487051ba
Vũ Tuân <75652084+vuhuyentuan@users.noreply.github.com>
2020-12-12 17:40:16 +0700

    Update PageController.php

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -22,13 +22,4 @@ class PageController extends Controller
     public function gettatcaSP (){
         return view('layout_index.page.viewall');
 }
-
-<<<<<<< HEAD
-  
-=======
-    public function getChitiet(){
-        return view('layout_index.page.product');
->>>>>>> 280df63ca4e41d7ea85c52c9f1fd9accb265d81b
-    }
->>>>>>> d1f2f8656ee11194b87198e4fe50808a5a70d0ae
 }
