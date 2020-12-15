8faffd7d735338a88c6fa2c77cac1e4a98ab6ddb
Vũ Tuân <75652084+vuhuyentuan@users.noreply.github.com>
2020-12-15 09:29:29 +0700

    productRepositry

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -13,26 +13,44 @@ class ProductRepository
      */
     public function getAll()
     {
-        return Product::paginate(10);
+        return Product::paginate(10)->orderby('created_at','desc');
     }
 
     /**
      * create a member.
      *
-     * @param  \App\Http\Requests\MemberRequest $request
-     * @param  \App\Models\Member $member
+     * @param  \App\Http\Requests\ProductRequest $request
+     * @param  \App\Models\Product $product
      * @return void
      */
-    public function create()
+    public function create($request)
     {
-       
+       //kiểm tra file tồn tại
+       $name="";
+       if($request->hasfile('img'))
+       {
+           $file = $request->file('img');
+           $name=time().'_'.$file->getClientOriginalName();
+           $destinationPath=public_path('image/product'); //project\public\image\cars, //public_path(): trả về đường dẫn tới thư mục public
+           $file->move($destinationPath, $name); //lưu hình ảnh vào thư mục public/image
+       }
+       $product = new Product();
+       $product->name=$request->input('name');
+       $product->id_type=$request->input('cate');
+       $product->unit_price=$request->input('unit_price');
+       $product->promotion_price=$request->input('promotion_price');
+       $product->description=$request->input('description');
+       $product->new=$request->input('featured');
+       $product->image=$name;
+       $product->save();
+       return redirect('list');
     }
 
     /**
      * update a member.
      *
-     * @param  \App\Http\Requests\MemberRequest $request
-     * @param  \App\Models\Member $member
+     * @param  \App\Http\Requests\ProductRequest $request
+     * @param  \App\Models\Product $product
      * @return void
      */
     public function update() {
@@ -43,8 +61,8 @@ class ProductRepository
      /**
      * delete a member.
      *
-     * @param  \App\Http\Requests\MemberRequest $request
-     * @param  \App\Models\Member $member
+     * @param  \App\Http\Requests\ProductRequest $request
+     * @param  \App\Models\Product $product
      * @return void
      */
     public function destroy() {
@@ -55,8 +73,8 @@ class ProductRepository
      /**
      * search  member.
      *
-     * @param  \App\Http\Requests\MemberRequest $request
-     * @param  \App\Models\Member $member
+     * @param  \App\Http\Requests\ProductRequest $request
+     * @param  \App\Models\Product $product
      * @return void
      */
 
