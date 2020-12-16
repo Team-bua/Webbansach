431d07d0b1ae43420e6409ca2ffde21fe4ed42bf
blackbullerk123 <65282322+blackbullerk123@users.noreply.github.com>
2020-12-16 17:06:17 +0700

    Nghichasdasdasd

---
+++ app/Repositories/NguyenvanteoResponsitory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repositories;
+
+use JasonGuru\LaravelMakeRepository\Repository\BaseRepository;
+//use Your Model
+
+/**
+ * Class NguyenvanteoResponsitory.
+ */
+class NguyenvanteoResponsitory extends BaseRepository
+{
+    /**
+     * @return string
+     *  Return the model
+     */
+    public function model()
+    {
+        //return YourModel::class;
+    }
+}
