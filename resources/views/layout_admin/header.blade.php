78c976ee5f2b1c401af789ed24b6e6c03ad7a8a5
Lâm Nguyễn <lamnguyen041297@gmail.com>
2020-12-19 12:06:47 +0700

    lam up

--- resources/views/layout_admin/header.blade.php
+++ resources/views/layout_admin/header.blade.php
@@ -14,7 +14,7 @@
         <li class="dropdown messages-menu">
           <a href="" class="dropdown-toggle" data-toggle="dropdown">
             <i class="fa fa-envelope-o"></i>
-            <span class="label label-success">4</span>
+            <span class="label label-primary">4</span>
           </a>
           <ul class="dropdown-menu">
             <li class="header">Bạn có 4 tin nhắn</li>
@@ -84,7 +84,7 @@
                 </li>
               </ul>
             </li>
-            <li class="footer"><a href="">See All Messages</a></li>
+            <li class="footer"><a href="">Xem thêm</a></li>
           </ul>
         </li>
         <!-- Notifications: style can be found in dropdown.less -->
@@ -94,7 +94,7 @@
             <span class="label label-warning">10</span>
           </a>
           <ul class="dropdown-menu">
-            <li class="header">You have 10 notifications</li>
+            <li class="header">Bạn có 10 thông báo</li>
             <li>
               <!-- inner menu: contains the actual data -->
               <ul class="menu">
@@ -126,83 +126,11 @@
                 </li>
               </ul>
             </li>
-            <li class="footer"><a href="">View all</a></li>
+            <li class="footer"><a href="">Xem thêm</a></li>
           </ul>
         </li>
-        <!-- Tasks: style can be found in dropdown.less -->
-        <li class="dropdown tasks-menu">
-          <a href="" class="dropdown-toggle" data-toggle="dropdown">
-            <i class="fa fa-flag-o"></i>
-            <span class="label label-danger">9</span>
-          </a>
-          <ul class="dropdown-menu">
-            <li class="header">You have 9 tasks</li>
-            <li>
-              <!-- inner menu: contains the actual data -->
-              <ul class="menu">
-                <li>
-                  <!-- Task item -->
-                  <a href="">
-                    <h3>
-                      Design some buttons
-                      <small class="pull-right">20%</small>
-                    </h3>
-                    <div class="progress xs">
-                      <div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                        <span class="sr-only">20% Complete</span>
-                      </div>
-                    </div>
-                  </a>
-                </li><!-- end task item -->
-                <li>
-                  <!-- Task item -->
-                  <a href="">
-                    <h3>
-                      Create a nice theme
-                      <small class="pull-right">40%</small>
-                    </h3>
-                    <div class="progress xs">
-                      <div class="progress-bar progress-bar-green" style="width: 40%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                        <span class="sr-only">40% Complete</span>
-                      </div>
-                    </div>
-                  </a>
-                </li><!-- end task item -->
-                <li>
-                  <!-- Task item -->
-                  <a href="">
-                    <h3>
-                      Some task I need to do
-                      <small class="pull-right">60%</small>
-                    </h3>
-                    <div class="progress xs">
-                      <div class="progress-bar progress-bar-red" style="width: 60%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                        <span class="sr-only">60% Complete</span>
-                      </div>
-                    </div>
-                  </a>
-                </li><!-- end task item -->
-                <li>
-                  <!-- Task item -->
-                  <a href="">
-                    <h3>
-                      Make beautiful transitions
-                      <small class="pull-right">80%</small>
-                    </h3>
-                    <div class="progress xs">
-                      <div class="progress-bar progress-bar-yellow" style="width: 80%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                        <span class="sr-only">80% Complete</span>
-                      </div>
-                    </div>
-                  </a>
-                </li><!-- end task item -->
-              </ul>
-            </li>
-            <li class="footer">
-              <a href="">View all tasks</a>
-            </li>
-          </ul>
-        </li>
+
+
         <!-- User Account: style can be found in dropdown.less -->
         <li class="dropdown user user-menu">
           <a href="" class="dropdown-toggle" data-toggle="dropdown">
@@ -296,14 +224,7 @@
         </a>
       </li>
       <!-- Quản lý tài khoản -->
-      <li class="treeview">
-        <a href="{{route('user.index')}}">
-          <i class="fa fa-user  fa-lg text-success"></i>
-          <span>Tài Khoản</span>
-         
-        </a>
 
-      </li>
 
       <!-- Quản lý kho hàng -->
       <li class="treeview">
@@ -314,7 +235,25 @@
         </a>
 
       </li>
+      <li class="header "  >
+        <span style="font-size:20px ;padding:20px;color:rgb(192, 184, 184)"> PHÂN QUYỀN </span></li>
 
+        <li class="treeview">
+          <a href="{{route('user.index')}}">
+            <i class="fa fa-desktop  fa-lg text-success"></i>
+            <span>Tài Khoản</span>
+           
+          </a>
+  
+        </li>
+        <li class="treeview">
+          <a href="">
+            <i class="fa fa-user  fa-lg text-success"></i>
+            <span>Vai trò</span>
+           
+          </a>
+  
+        </li>
       <!--
             <li class="treeview">
               <a href="">
