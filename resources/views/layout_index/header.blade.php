4e3b57ab06b32181861e0af521ed62d29d6fd763
Vũ Tuân <75652084+vuhuyentuan@users.noreply.github.com>
2021-01-06 16:23:49 +0700

    Update header.blade.php

--- resources/views/layout_index/header.blade.php
+++ resources/views/layout_index/header.blade.php
@@ -2,23 +2,9 @@
     <div class="header-top">
         <div class="container">
             <div class="row">
-<<<<<<< HEAD
-                @if (Auth::check())
-                    <div class="col-md-3">Chào Bạn : <a
-                            href="{{ route('admin') }}">{{ Auth::user()->full_name }}</a></a><br><a
-                            href="{{ url('logout') }}">Đăng Xuất</a></div>
-                @endif
-=======
->>>>>>> 30eefb03d382412192f74532ef34f34b24b6746a
-
                 <div class="col-md-3">
                     <span class="ph-number"><i class="fa fa-truck"></i> Miễn Phí Vận Chuyển </span>
                 </div>
-<<<<<<< HEAD
-                <div class="col-md-3">
-                    <span class="ph-number"><i class="fa fa-phone"></i>Call: 0779 750 123</span>
-                </div>
-=======
                 <div class="col-md-4 ">
                     <span style="float: left;" class="ph-number"><i class="fa fa-phone"></i>Số điện thoại: 0779 750 123
                         / 0945 555 666</span>
@@ -34,82 +20,19 @@
 
                     @endif
                 @endif
-
->>>>>>> 30eefb03d382412192f74532ef34f34b24b6746a
             </div>
         </div>
     </div>
     <div class="main-menu">
         <div class="container">
             <nav class="navbar navbar-expand-lg navbar-light">
-<<<<<<< HEAD
-                <a class="navbar-brand" href="{{ route('index') }}"><img src="images/logoteambua.png" alt="logo"></a>
-=======
                 <a class="navbar-brand text-center" href="{{ route('index') }}"><img style=" height: 140px ;width:50%"
                         src="images/logobook.png" alt="logo"></a>
->>>>>>> 30eefb03d382412192f74532ef34f34b24b6746a
                 <button class="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
-<<<<<<< HEAD
-                
-            </nav>
-        </div>
-        <div class="main-menu">
-          <div class="container">
-              <nav class="navbar navbar-expand-lg navbar-light">
-                   <div id="menu">
-    <ul>
-      <li class="menu_item down"><a href="#"><i class="fa fa-bars"></i> Danh Mục Sách</a>
-                              <div class="sub_menu" style="">
-                                  <div class="bubble"></div>
-                                  <div class="sub_menu_block" style="width:326px">
-                                      <ul>
-                                          @foreach ($types as $pro)
-                                              <li><a href="{{ route('product_type',$pro->id) }}">{{ $pro->name }}</a></li>
-                                          @endforeach                            
-                                      </ul>
-
-                                  </div>
-                              </div>
-                          </li>
-      <li class="menu_item down"><a href="{{ route('index') }}">Trang Chủ</a> </li>
-      <li class="menu_item down"><a href="{{ route('introduce') }}">Giới thiệu</a></li>
-      <li class="menu_item down"><a href="{{ route('news') }}">Tin Tức</a></li>
-      @if (Auth::check())
-      @else
-      <li class="menu_item down"><a href="{{ route('login') }}">Đăng Nhập</a></li>
-      <li class="menu_item down"><a href="{{ route('signup') }}">Đăng Ký</a></li>
-      @endif
-    </ul>
-  </div>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent" style="margin-left: 20%">
-                    <div class="cart my-2 my-lg-0">
-                        <a href="{{ route('cart') }}">
-                            <span><i class="fa fa-shopping-cart" aria-hidden="true"></i></span></a>
-                        <span class="quntity">
-                            @if (Session::has('cart'))
-                            {{ Session('cart')->totalQty }}@else 0
-                            @endif
-                        </span>
-                    </div>
-                    <form class="form-inline my-2 my-lg-0" role="search" method="get" id="searchform"
-                        action="{{ route('search') }}">
-                        <input type="text" value="" name="key" id="s" placeholder="Nhập từ khóa..."
-                            autocomplete="off" />
-                        <span class="fa fa-search"></span>
-                    </form>
-                </div>
-              </nav>
-
-          </div>
-
-      </div>
-    </header>
-=======
 
             </nav>
         </div>
@@ -167,4 +90,4 @@
         </div>
     </div>
 </header>
->>>>>>> 30eefb03d382412192f74532ef34f34b24b6746a
+
