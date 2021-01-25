4bee00649254da46bca481011021c3191e768b5d
Vũ Tuân <75652084+vuhuyentuan@users.noreply.github.com>
2021-01-25 11:54:06 +0700

    lõi cơ ban

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -49,16 +49,16 @@ class Cart
 	}
 
 	public function changeItem($id,$quantity){
-		$quantitys=(int)$quantity;
-		$this->totalQty -= $this->items[$id]['qty'];
-		$this->totalPrice -= $this->items[$id]['price'];
-
-		$this->items[$id]['qty'] = $quantitys;
-		$this->items[$id]['price'] = $quantitys*($this->items[$id]['item']->promotion_price==0?$this->items[$id]['item']->unit_price:$this->items[$id]['item']->promotion_price);
-		
-		$this->totalQty += $this->items[$id]['qty'];
-		$this->totalPrice += $this->items[$id]['price'];
-	}
+		if($quantity > 0){
+			$quantitys=(int)$quantity;
+			$this->totalQty -= $this->items[$id]['qty'];
+			$this->totalPrice -= $this->items[$id]['price'];
 	
-
+			$this->items[$id]['qty'] = $quantitys;
+			$this->items[$id]['price'] = $quantitys*($this->items[$id]['item']->promotion_price==0?$this->items[$id]['item']->unit_price:$this->items[$id]['item']->promotion_price);
+			
+			$this->totalQty += $this->items[$id]['qty'];
+			$this->totalPrice += $this->items[$id]['price'];
+		}
+	}	
 }
